7f161144c203f393645f82bd58cdf4083158fc34
GamerNoTitle <bili33@87ouo.top>
2021-07-05 20:15:30 +0800

    Action fix

--- Downloader-Action.py
+++ Downloader-Action.py
@@ -49,26 +49,6 @@ elif steamcustomid != '':
 else:  
     CompetitionStatsLink = False
 
-def progressbar(url,path):
-    start = time.time() 
-    response = r.get(url, stream=True) 
-    size = 0    
-    chunk_size = 1024  
-    content_size = int(response.headers['content-length'])  
-    try:
-        if response.status_code == 200:   
-            print('[INFO]Start download, File size:{size:.2f} MB'.format(size = content_size / chunk_size /1024))   
-            filepath = path  
-            with open(filepath,'wb') as file:   
-                for data in response.iter_content(chunk_size = chunk_size):
-                    file.write(data)
-                    size +=len(data)
-                    print('\r'+'[INFO]Downloading: %s%.2f%%' % ('>'*int(size*50/ content_size), float(size / content_size * 100)) ,end=' ')
-        end = time.time()
-        print('[INFO]Download completed! Times: %.2f seconds' % (end - start))
-    except:
-        print('[INFO]Download failed.')
-
 def Download():
     if not CompetitionStatsLink:
         print('[ERROR]Neither steamid64 nor steamcustomid has been set. The program will exit now.')
@@ -92,10 +72,9 @@ def Download():
                 print('[INFO]Demo file {} exists, skipping download process.'.format(filename))
             else:
                 print('[INFO]Downloading {} from {}'.format(filename,filelink.replace('\\','').replace('"','')))
-                progressbar(filelink.replace('\\','').replace('"',''),'./Demo/'+filename)
-                # with open('./Demo/'+filename,'wb+') as f:
-                #     f.write(r.get(filelink.replace('\\','').replace('"','')).content)
-                #     f.close
+                with open('./Demo/'+filename,'wb+') as f:
+                    f.write(r.get(filelink.replace('\\','').replace('"','')).content)
+                    f.close
         if previousdownload == 'true' or previousdownload == 'True':
             print('[INFO]Previous download mode is enabled. Download process will continue.')
             CompetitionList = r.get(CompetitionStatsLink+'&continue_token={}&sessionid={}'.format(continuetoken,sessionid),headers=headers).text
