65ca030d5c5a6b213b2aabc57b318f7c68fd1af3
GamerNoTitle <bili33@87ouo.top>
2021-07-06 10:36:12 +0800

    Bug fixed and English localize

--- Downloader.py
+++ Downloader.py
@@ -10,18 +10,6 @@ with open('./config.json','r') as f:
     config=js.load(f)
     f.close()
 
-WelcomeMsg='''======================== Counter-Strike: Global Offensive Demo Downloader ========================
-Thanks for your using on CSGO-Demo-AutoDownloader, This is a automatic downloader that helps you download your newest CSGO demos
-The project is maintained by GamerNoTitle. If this project helps you, consider donate please.
-
-Paypal: https://paypal.me/GamerNoTitle
-afdian: https://afdian.net/@GamerNoTitle
-Alipay (QR Code): https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/AliPay.jpg
-WechatPay (QR Code): https://cdn.jsdelivr.net/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/WeChat.png
-==================================================================================================
-'''
-
-print(WelcomeMsg)
 lang = config['lang']
 steamcustomid = config['steamcustomid']
 steamid64 = config['steamid64']
@@ -80,17 +68,17 @@ def progressbar(url,path):
     content_size = int(response.headers['content-length'])  
     try:
         if response.status_code == 200:   
-            print('[INFO]Start download, File size:{size:.2f} MB'.format(size = content_size / chunk_size /1024))   
+            print(LangString('info.download.start').format(size = content_size / chunk_size /1024))   
             filepath = path  
             with open(filepath,'wb') as file:   
                 for data in response.iter_content(chunk_size = chunk_size):
                     file.write(data)
                     size +=len(data)
-                    print('\r'+'[INFO]Downloading: %s%.2f%%' % ('>'*int(size*50/ content_size), float(size / content_size * 100)) ,end=' ')
+                    print('\r'+ LangString('info.download.downloading') +'%s%.2f%%' % ('>'*int(size*50/ content_size), float(size / content_size * 100)) ,end=' ')
         end = time.time()
-        print('\n[INFO]Download completed! Times: %.2f seconds' % (end - start))
+        print('\n' + LangString('info.download.complete') % (end - start))
     except Exception as e:
-        print('[WARN]Download failed. {}'.format(e))
+        print( LangString('warn.download.failed').format(e))
 
 if steamcustomid == '' and steamid64 != 0:
     CompetitionStatsLink = "https://steamcommunity.com/profile/{}/gcpd/730?ajax=1&tab=matchhistorycompetitive".format(steamid64)
@@ -102,20 +90,20 @@ else:
 def Download():
     global CompetitionStatsLink
     if not CompetitionStatsLink:
-        print('[ERROR]Neither steamid64 nor steamcustomid has been set. The program will exit now.')
+        print(LangString('error.idnotset'))
         sys.exit()
     if(proxies['http']=='' and proxies['https']==''):
-        print('[WARN]Proxies not set yet, the program will not run as proxy mode.')
+        print(LangString('warn.proxies.disabled'))
         CompetitionList = r.get(CompetitionStatsLink, headers=headers).text
     else:
-        print('[INFO]Proxies have been set, the program will run as proxy mode')
+        print(LangString('info.proxies.enabled'))
         proxyenabled=True
         CompetitionList = r.get(CompetitionStatsLink, proxies=proxies, headers=headers).text
     while True:
         LinkList=[]
         LinkList = re.findall(r'"http:\\\/\\\/replay141\.valve\.net\\\/730\\\/................................\.dem\.bz2', CompetitionList)
         if LinkList == []:
-            print('[INFO]There\'s no any demo to download, download process accomplished. The program will now sleep for {} seconds and work again.'.format(delay))
+            print(LangString('info.download.nodemo').format(delay))
             if steamcustomid == '' and steamid64 != 0:
                 CompetitionStatsLink = "https://steamcommunity.com/profile/{}/gcpd/730?ajax=1&tab=matchhistorycompetitive".format(steamid64)
             elif steamcustomid != '':
@@ -125,30 +113,34 @@ def Download():
             time.sleep(delay)
         try:
             continuetoken = str(re.findall(r'\"continue_token\"\:\"[0-9]+\"', CompetitionList)[0]).replace('"','').replace('continue_token:','')
-            print('[INFO]New continue token has been set. If you enabled previous download mode, this token will work.')
+            print(LangString('info.continuetoken.set'))
         except:
-            print('[WARN]continue token not found. Don\'t worry, we will use the previous one.')
+            print(LangString('warn.continuetoken.notfound'))
         for link in LinkList:
             filelink=link
             filename=link[40:]
             if os.path.exists('./Demo/'+filename):
-                print('[INFO]Demo file {} exists, skipping download process.'.format(filename))
+                print(LangString('info.file.exists').format(filename))
             else:
-                print('[INFO]Downloading {} from {}'.format(filename,filelink.replace('\\','').replace('"','')))
+                print(LangString('info.file.downloading').format(filename,filelink.replace('\\','').replace('"','')))
                 progressbar(filelink.replace('\\','').replace('"',''),'./Demo/'+filename)
                 # with open('./Demo/'+filename,'wb+') as f:
                 #     f.write(r.get(filelink.replace('\\','').replace('"','')).content)
                 #     f.close
         if previousdownload:
-            print('[INFO]Previous download mode is enabled. Download process will continue.')
+            print(LangString('info.previousdl.enabled'))
             if proxyenabled:
                 CompetitionList = r.get(CompetitionStatsLink+'&continue_token={}&sessionid={}'.format(continuetoken,sessionid),proxies=proxies,headers=headers).text
             else:
                 CompetitionList = r.get(CompetitionStatsLink+'&continue_token={}&sessionid={}'.format(continuetoken,sessionid),headers=headers).text
         else:
-            print('[INFO]Previous download mode is disabled. The program will only download the latest 4 competitions.')
+            print(LangString('info.previousdl.disabled'))
+            print(LangString('info.download.nodemo').format(delay))
+            time.sleep(delay)
             
 
 if __name__ == '__main__':
+    WelcomeMsg=LangString('info.welcomemsg')
+    print(WelcomeMsg)
     Download()
-    input("\nPress ENTER to continue...")
+    input("\n" + LangString('tips.continue'))
