24ef589c73abee5c3bfed00ca9fe079ee438aee4
GamerNoTitle <bili33@87ouo.top>
2021-07-06 13:54:19 +0800

    Added Sentry Track Function (Not packed in RELEASE Version)

--- Downloader.py
+++ Downloader.py
@@ -6,15 +6,15 @@ from http.cookies import SimpleCookie
 import os
 import sys
 
-# import sentry_sdk
-# sentry_sdk.init(
-#     "https://3a21870186ec4fd5902051bb922a0458@o361988.ingest.sentry.io/5850334",
+import sentry_sdk
+sentry_sdk.init(
+    "https://3a21870186ec4fd5902051bb922a0458@o361988.ingest.sentry.io/5850334",
 
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0
-# )
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 with open('./config.json', 'r') as f:
     config = js.load(f)
